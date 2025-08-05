7ecceac02ff24ecfef81556b79d2dcf7880b1794
DillonIs <dillonis826@gmail.com>
2025-08-05 13:16:08 +1000

    Testing more of part 3 code

--- Part_3/run_v4.py
+++ Part_3/run_v4.py
@@ -10,7 +10,7 @@ from app.api.v1.auth import api as auth_ns
 from app.api.v1.protected import api as protected_ns
 from flask_cors import CORS
 
-# app = create_app()
+app = create_app()
 
 app = Flask(__name__)
 
